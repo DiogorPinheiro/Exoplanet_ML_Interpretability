2cd8615b894c43f7c5ee6f1788a29bea9c60d123
DiogorPinheiro <diogomrpinheiro@gmail.com>
2020-05-29 16:42:55 +0100

    earth++

--- interpreter.py
+++ interpreter.py
@@ -33,7 +33,8 @@ global_X = []
 
 def new_predict(data):
     y_pred = model.predict(data).reshape(-1, 1)
-    y_pred = (y_pred > 0.5)
+    y_pred = (y_pred > 0.5),
+    print(y_pred)
     # print(np.array(
     #    list(zip(1-y_pred.reshape(data.shape[0]), y_pred.reshape(data.shape[0])))))
     return np.hstack((1-y_pred, y_pred))
@@ -71,27 +72,26 @@ if __name__ == "__main__":
     for index, value in enumerate(test_Y):
         if value == prediction[index] and value == 1:
             correct_predictions.append(index)
-    # print(correct_predictions)
 
+    print(correct_predictions)
     # -------------------------------------------------------------------------------
 
     # evaluation(CNN_MODEL_DIRECTORY, test_X_shaped, test_Y)  # Evaluate Model
 
     # PC : 1, 4, 8, 9, 11, 13, 14, 16, 23, 27            |  !PC: 0, 2, 3, 5, 6, 7, 10, 12, 15, 17
 
-    num_features_set = [5, 10, 15, 20]
-    num_slices_set = [10, 20, 30, 40]
+    '''
     #idx = correct_predictions[0]
-    idx = 4
-    groups = createGroups(test_X_shaped[idx], 40, 5)
-    #series = test_X_shaped[idx]
+    idx = 2
+    #groups = createGroups(test_X_shaped[idx], 40, 5)
+    series = test_X_shaped[idx]
     #series = cloneSegment(2, 4, groups, test_X_shaped, idx)
-    series = shiftSegments(2, 4, groups, test_X_shaped, idx)
+    #series = shiftSegments(2, 4, groups, test_X_shaped, idx)
 
     explainer = LimeTimeSeriesExplanation(
         class_names=['0', '1'], feature_selection='auto')
     exp = explainer.explain_instance(series, new_predict, num_features=5, num_samples=5000, num_slices=40,
-                                     replacement_method='total_mean', training_set=train_X_shaped, top_labels=1)
+                                     replacement_method='total_mean', training_set=train_X_shaped, labels=(0, 1))
 
     values_per_slice = math.ceil(len(series) / 40)
     plt.plot(series, color='b', label='Explained instance')
@@ -99,13 +99,27 @@ if __name__ == "__main__":
              color='green', label='Mean of other class')
     plt.legend(loc='lower left')
     for i in range(5):
-        feature, weight = exp.as_list()[0]
+        feature, weight = exp.as_list(label=1)[0]
         start = feature * values_per_slice
         end = start + values_per_slice
         plt.axvspan(start, end, color='red', alpha=abs(weight*2))
-        plt.savefig('shifted_try2_1.png')
-
+        plt.savefig('pos.png')
     '''
+
+    num_features_set = [5, 10, 15, 20]
+    num_slices_set = [10, 20, 30, 40]
+    correct_predictions = [0, 4, 8, 11, 98, 106, 132, 164, 235, 477, 2242]
+    '''
+    for f in range(len(test_X_shaped)):
+        plt.plot(test_X_shaped[f],
+                 color='b', label='Explained instance')
+        if test_Y[f] == 1:
+            plt.savefig('Images/Light_Curves/Ones_'+str(f)+'.png')
+        else:
+            plt.savefig('Images/Light_Curves/Zeros_'+str(f)+'.png')
+        plt.close()
+    '''
+
     for slices in num_slices_set:
 
         for feat in num_features_set:
@@ -119,7 +133,7 @@ if __name__ == "__main__":
                 explainer = LimeTimeSeriesExplanation(
                     class_names=['0', '1'], feature_selection='auto')
                 exp = explainer.explain_instance(series, new_predict, num_features=num_features, num_samples=5000, num_slices=num_slices,
-                                                 replacement_method='total_mean', training_set=train_X_shaped, top_labels=1)
+                                                 replacement_method='total_mean', training_set=train_X_shaped, labels=(0, 1))
                 # print(exp.available_labels()[0])
                 # print(exp.as_list(label=1))
 
@@ -133,7 +147,7 @@ if __name__ == "__main__":
                     start = feature * values_per_slice
                     end = start + values_per_slice
                     plt.axvspan(start, end, color='red', alpha=abs(weight*2))
-                plt.savefig("slices_"+str(slice)+"feat_" +
+                plt.savefig("slices_"+str(slices)+"feat_" +
                             str(feat)+"/"+str(idx)+'_positive.png')
                 plt.close()
 
@@ -144,7 +158,7 @@ if __name__ == "__main__":
                 explainer = LimeTimeSeriesExplanation(
                     class_names=['0', '1'], feature_selection='auto')
                 exp = explainer.explain_instance(series, new_predict, num_features=num_features, num_samples=5000, num_slices=num_slices,
-                                                 replacement_method='total_mean', training_set=train_X_shaped, top_labels=0)
+                                                 replacement_method='total_mean', training_set=train_X_shaped, labels=(0, 1))
                 # print(exp.available_labels()[0])
                 # print(exp.as_list(label=1))
 
@@ -158,7 +172,6 @@ if __name__ == "__main__":
                     start = feature * values_per_slice
                     end = start + values_per_slice
                     plt.axvspan(start, end, color='red', alpha=abs(weight*2))
-                plt.savefig("slices_"+str(slice)+"feat_" +
+                plt.savefig("slices_"+str(slices)+"feat_" +
                             str(feat)+"/"+str(idx)+'_negative.png')
                 plt.close()
-    '''
